dbac0027d7296cc36edee0143c434007c9a8df93
Sachin kumar <110657130+Sachin-RJ7@users.noreply.github.com>
2022-11-18 22:28:45 +0500

    = Added review section slider =

--- script.js
+++ script.js
@@ -13,3 +13,16 @@ var swiper = new Swiper(".mySwiper", {
       prevEl: ".swiper-button-prev",
     },
   });
+
+// review swiper
+  var swiper = new Swiper(".mySwiper2", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    slidesPerGroup: 1,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
