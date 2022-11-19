23e5149fd06fbc6dfdca499ca087563a705dc741
Sachin kumar <110657130+Sachin-RJ7@users.noreply.github.com>
2022-11-19 20:34:54 +0500

    = added slider1 for hero section  =

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+
+// hero section
+var swiper = new Swiper(".mySwiper1", {
+  slidesPerView: 1,
+  spaceBetween: 0,
+  slidesPerGroup: 1,
+  loop: true,
+  loopFillGroupWithBlank: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
 var swiper = new Swiper(".mySwiper", {
     slidesPerView: 4,
     spaceBetween: 10,
